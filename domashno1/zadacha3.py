d64c14a835410755a6a9ba6e00d9f2bddec9bd91
LiliaG123 <liliageorgieva411@gmail.com>
2025-10-05 14:36:27 +0300

    zadacha 3

--- domashno1/zadacha3.py
+++ domashno1/zadacha3.py
@@ -1,1 +1,12 @@
+words = ["level", "python", "radar", "java", "civic", "kotlin", "refer"]
+palindromes = []
 
+for word in words:
+    backward = ""   
+    for letter in word:  
+        backward = letter + backward
+
+    if word == backward:   
+        palindromes.append(word)
+
+print(palindromes)
